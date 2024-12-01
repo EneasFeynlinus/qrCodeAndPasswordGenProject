2e566ec8a886437ebaef6b6b05fd6b63145f7f5c
Eneas Junior <eneas.silva.jr@gmail.com>
2024-12-01 19:24:28 -0300

    Configured main prompt from library to be called on index to produce out app prompt

---
+++ src/prompts/prompt-main.js
@@ -0,0 +1,15 @@
+import chalk from "chalk";
+
+// import { message } from "prompt";
+const mainPrompt = [
+    {
+        name: "select",
+        description: chalk.bold("Escolha a ferramenta (1 - QrCode ou (2- Password"),
+        pattern: /^[1-2]+$/,
+        message: chalk.red.italic("Escolha apenas entre 1 e 2"),
+        required: true,//significa que é obrigatório
+
+    },
+];
+
+export default mainPrompt
